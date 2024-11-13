ac9f674a5fd53c30a9dd1ac617e8ec1bbe99277d
Carla Menegat <146187896+CarlaMenegat@users.noreply.github.com>
2024-11-13 12:17:22 +0100

    .

--- graph_class.py
+++ graph_class.py
@@ -1,11 +1,58 @@
+from csv import reader
+
+with open("meta.csv", "r", encoding="utf-8") as f:
+    meta = reader(f)
+
+from pprint import pprint
+
+pprint (type(meta))
+        
+with open("meta.csv", "r", encoding="utf-8") as f:
+    meta = reader(f)
+    next(meta) 
+    
+    for row in meta:
+        pprint(row)
+
+with open("meta.csv", "r", encoding="utf-8") as f:
+    meta = reader(f)
+    
+    print("--First iteration")
+    for row in meta:
+        pprint(row)   
+    
+    print("\n-- Second iteration")
+    for row in meta:
+        pprint(row)  
+        
+with open("meta.csv", "r", encoding="utf-8") as f:
+    meta = reader(f)
+    meta_list = list(meta)
+    
+print("-- First execution")
+for row in meta_list:
+    pprint(row)  
+
+print("\n-- Second executuion")
+for row in meta_list:
+    pprint(row)
+
 class Person(object):
     def __init__ (self, name):
         self.name = name
         
     def getName(self):
         return self.name
-        
 
+my_meta_list =[ 
+    ["column name", "column name", "column name", "column name", "column name", "column name"],
+
+    ]
+pprint (my_meta_list)
+
+ninth_row = my_meta_list[8]
+print("-- ninth row")
+pprint(ninth_row)
 
 class CulturalHeritageObject(object):
     def __init__(self,title, date, owner, place):
