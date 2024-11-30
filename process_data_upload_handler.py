0308a64ec54a189961c96f5928181a1a630a999c
matildepassafaro <matilde__passafaro@outlook.it>
2024-11-30 18:22:08 +0100

    process data upload handler

--- process_data_upload_handler.py
+++ process_data_upload_handler.py
@@ -1,33 +1,156 @@
 from handler import Handler, UploadHandler
 import pandas as pd
+from pandas import DataFrame
+from pandas import Series
 from json import load
-from json import dump
+from sqlite3 import connect
 
 class ProcessDataUploadHandler(UploadHandler):
     pass
 
-    # open json file
-    with open("data/process.json", "r", encoding="utf-8") as f:
-        json_data = load(f)
+    # the method of ProcessDataUploadHandler that pushes data to the rel db
+    def pushDataToDb(self, file_path):
+
+        # open json file
+        with open("data/process.json", "r", encoding="utf-8") as f:
+            json_data = load(f)
     
-    # extract acquisition information
-    acquisitions = []
-    for item in json_data:
-        acquisitions.append(item["acquisition"])
+        # function for extracting data from json
+        def data_from_json(json_object, dict_key):
+            # initialize list empty
+            process = []
+            # set counter
+            count = 0
+            # iterate over dictionaries in the input list
+            for item in json_object:
+                # add 1 to the counter at each iteration
+                count += 1
+                # iterate over key,value pairs for each dictionary
+                for key, val in item.items():
+                    # check if key is equal to the input key
+                    if key == dict_key:
+                        # if it is, append val (the inner dictionary) to the process list
+                        process.append(val)
+                        # and add the object id to the inner dictionary
+                        val.update(refers_to="object_" + str(count))
+            
+            # return the list        
+            return process
+
+        # function calls
+        acquisition = data_from_json(json_data, "acquisition")
+        processing = data_from_json(json_data, "processing")
+        modelling = data_from_json(json_data, "modelling")
+        optimising = data_from_json(json_data, "optimising")
+        exporting = data_from_json(json_data, "exporting")
+
+        print("Acquisition list:\n", acquisition)
+
+        # function for populating dataframes from lists
+        def populateDf(process_list): # l'errore delle 70 righe viene generato qui
+        
+            df = pd.DataFrame(process_list)
+            # iterate over columns in the df for associating datatype
+            for column_name, column in df.items():
+                if column_name == "tool":
+                    df = df.astype("string")
+                else:
+                    df = df.astype(dtype={"tool": "object"})
+            return df
+
+        # function calls
+        acquisition_df = populateDf(acquisition)
+        processing_df = populateDf(processing)
+        modelling_df = populateDf(modelling)
+        optimising_df = populateDf(optimising)
+        exporting_df = populateDf(exporting)
+
+        print("Acquisition dataframe:\n", acquisition_df)
+        print("Acquisition dataframe info:", acquisition_df.info())
+
+        # create unique identifiers and append id column to df
+        def createUniqueId(process_df, df_name):
+            id = []
+            # iterate over dataframe rowa
+            for idx, row in process_df.iterrows():
+                # at each iteration, append to the list a string composed by dataframe name + underscore + the index of the row to be used as unique identifier
+                id.append(str(df_name) + "_" + str(idx))
+
+            # convert the list to a Series and insert it as first column in the df, with name "unique_id"
+            process_df.insert(0, "unique_id", Series(id, dtype="string"))
+
+            return process_df
+
+        # function calls
+        createUniqueId(acquisition_df, "acquisition")
+        createUniqueId(processing_df, "processing")
+        createUniqueId(modelling_df, "modelling")
+        createUniqueId(optimising_df, "optimising")
+        createUniqueId(exporting_df, "exporting")
+
+        print("Acquisition df with unique ids:\n", acquisition_df)
+
+        # remove multi-valued attributes from df
+        def keep_single_valued(process_df):
+            # dtypes stores a series where the first column lists the index for the Series (the column names) and the second one the datatype for each column
+            dtypes = process_df.dtypes
+            print(isinstance(dtypes, pd.Series))
+            # iterate over the columns in the Series
+            for column_name, datatype in dtypes.items():
+                # if the column has datatype object...
+                if datatype == object:
+                    #... pop the column from the df
+                    multi_valued = process_df.pop(column_name)
+
+            # return the df and the popped column
+            return process_df, multi_valued
     
-    print(acquisitions)
+        # function calls
+        acquisition_df, acquisition_multi_valued = keep_single_valued(acquisition_df)
+        processing_df, processing_multi_valued = keep_single_valued(processing_df)
+        modelling_df, modelling_multi_valued = keep_single_valued(modelling_df)
+        optimising_df, optimising_multi_valued = keep_single_valued(optimising_df)
+        exporting_df, exporting_multi_valued = keep_single_valued(exporting_df)
+        print("Acquisition df and multi-valued column:\n", acquisition_df, acquisition_multi_valued)
+        print(acquisition_df.info())
 
-    # dump acquisition info into a new json file
-    with open("data/acquisition_data.json", "w", encoding="utf-8") as f:
-        dump(acquisitions, f, ensure_ascii=False, indent=4)
+        # pushing tables to db
+        with connect("relational.db") as con:
+            acquisition_df.to_sql("Acquisition", con, if_exists="replace", index=False)
+            processing_df.to_sql("Processing", con, if_exists="replace", index=False)
+            modelling_df.to_sql("Modelling", con, if_exists="replace", index=False)
+            optimising_df.to_sql("Optimising", con, if_exists="replace", index=False)
+            exporting_df.to_sql("Exporting", con, if_exists="replace", index=False)
+        
+            # check and return result
+            rel_db_ac = pd.read_sql("SELECT * FROM Acquisition", con)
+            rel_db_pr = pd.read_sql("SELECT * FROM Processing", con)
+            rel_db_md = pd.read_sql("SELECT * FROM Modelling", con)
+            rel_db_op = pd.read_sql("SELECT * FROM Optimising", con)
+            rel_db_ex = pd.read_sql("SELECT * FROM Exporting", con)
 
-    # open new file in a pd dataframe
-    acquisition_data = pd.read_json("data/acquisition_data.json", dtype={"responsible institute": "string", "responsible person": "string", "technique": "string", "tool": "object", "start date": "string", "end date": "string"})
-    print(acquisition_data)
+            populated_tables = not any(df.empty for df in [rel_db_ac, rel_db_pr, rel_db_op, rel_db_md, rel_db_ex])
+            print(populated_tables)
+            return populated_tables
 
-    # extract single-valued attributes
-    acquisition_table = acquisition_data[["responsible institute", "technique"]]
-    print(acquisition_table)
 
-    # now tables for multi-valued attributes + primary keys + table for CHO
+# for uploading data to db (and testing)
+rel_path = "relational.db"
+process = ProcessDataUploadHandler()
+process.setDbPathOrUrl(rel_path)
+process.pushDataToDb("data/process.json")
+
+    
+
+
+
+
+
+
+
+
+    
+    
+
+    
 
