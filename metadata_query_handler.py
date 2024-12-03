161c8ccf04618c29f2b571ef1cc09a1c0c8374ff
alicepiazzi <alicep2401@gmail.com>
2024-12-03 13:33:20 +0100

    viaf and ulan prefix

--- metadata_query_handler.py
+++ metadata_query_handler.py
@@ -1,24 +1,30 @@
+from SPARQLWrapper import SPARQLWrapper, JSON  # Assicurati che JSON sia importato
 import pandas as pd
-import SPARQLWrapper
 from query_handler import QueryHandler
-from handler import Handler, UploadHandler
-from metadata_upload_handler import MetadataUploadHandler
 
 class MetadataQueryHandler(QueryHandler):
     def __init__(self):
+        super().__init__()
         
-    
     def getAllPeople(self):
         sparql = SPARQLWrapper(self.getDbPathOrUrl())
         sparql.setQuery("""
-            PREFIX ex: <http://example.org/> #cambiare per qualcosa com VIAF or ULAN
-            SELECT ?personId ?name #person id bisogna essere Viaf number or Ulan number
+            PREFIX ulan: <http://vocab.getty.edu/ulan/>  # Prefisso ULAN
+            PREFIX viaf: <http://viaf.org/viaf/>        # Prefisso VIAF
+
+            SELECT ?personId ?name  # personId può essere un numero ULAN o VIAF
             WHERE {
-                ?personId a ex:Person ; 
-                ex:name ?name .
+                {
+                    ?personId a ulan:Person ;  # Dati da ULAN
+                    ulan:name ?name .
+                }
+                UNION
+                {
+                    ?personId a viaf:Person ;  # Dati da VIAF
+                    viaf:name ?name .
+                }
             }
         """)
         sparql.setReturnFormat(JSON)
         results = sparql.query().convert()
         return pd.DataFrame([{"id": result["personId"]["value"], "name": result["name"]["value"]} for result in results["results"]["bindings"]])
-    
